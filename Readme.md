adf065ae55f9d9c1d8e6f758b7aa5924417586cf
shaga <gagsh1812@gmail.com>
2019-11-10 14:30:00 +0530

    adding files

---
+++ Readme.md
@@ -0,0 +1,5 @@
+## Dog-Breed Classifier App
+
+Built using Pytorch and Flask, this project extends the work done by Avinash Sajjanshetty at https://github.com/avinassh/pytorch-flask-api-heroku 
+
+
---
+++ commons.py
@@ -0,0 +1,53 @@
+import io
+import torch
+import torch.nn as nn
+
+from PIL import Image
+from torchvision import models
+import torchvision.transforms as transforms
+
+model_link ='https://www.dropbox.com/s/03ljptd8syhyeqr/model_transfer.pt?dl=1'
+async def download_file(url, dest):
+    if dest.exists(): return
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            data = await response.read()
+            with open(dest, 'wb') as f:
+                f.write(data)
+
+def get_model():
+    model = models.vgg16(pretrained=False)
+    for param in model.parameters():
+        param.requires_grad = False
+    classifier = nn.Sequential(nn.Linear(25088, 4096),
+                           nn.ReLU(),
+                           nn.Dropout(0.5),
+                           nn.Linear(4096, 512),
+                           nn.ReLU(),
+                           nn.Dropout(0.5),
+                           nn.Linear(512, 133))
+    model.classifier = classifier
+    download_file(model_link,'model_transfer.pt')
+    model.load_state_dict(torch.load('model_transfer.pt', map_location=torch.device('cpu')))
+    model.eval()
+    return model
+
+
+def transform_image(image_bytes):
+    my_transforms = transforms.Compose([transforms.Resize(255),
+                                        transforms.CenterCrop(224),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize(
+                                            [0.485, 0.456, 0.406],
+                                            [0.229, 0.224, 0.225])])
+    image = Image.open(io.BytesIO(image_bytes))
+    return my_transforms(image).unsqueeze(0)
+
+
+# ImageNet classes are often of the form `can_opener` or `Egyptian_cat`
+# will use this method to properly format it so that we get
+# `Can Opener` or `Egyptian Cat`
+def format_class_name(class_name):
+    class_name = class_name.replace('_', ' ')
+    class_name = class_name.title()
+    return class_name
